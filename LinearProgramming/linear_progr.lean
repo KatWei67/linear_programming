269b13c73db532b831e51d0a59100a6d26de590b
KatWei67 <jwei7@uw.edu>
2024-04-16 12:55:48 -0700

    Added files from previous repo.

---
+++ LinearProgramming/linear_progr.lean
@@ -0,0 +1,13 @@
+import Mathlib.Analysis.InnerProductSpace.PiL2
+import Mathlib.Data.Matrix.Basic
+import Mathlib.Data.Matrix.Reflection
+import Mathlib.Data.Matrix.RowCol
+
+open Matrix
+variable(m n :ℕ)
+variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)(v₁: (Fin m) → ℝ )(i: Fin m)
+#check M *ᵥ v  -- works
+#check row v
+#check col v
+#check (row v₁) * M
+#check col (M i)
