13ef06b8a51e452b56dd0f8b653e254c33f16ce7
christieyyx <yyang46@uw.edu>
2024-04-17 13:58:24 -0700

    set defined

--- LinearProgramming/linear_progr.lean
+++ LinearProgramming/linear_progr.lean
@@ -5,15 +5,20 @@ import Mathlib.Data.Matrix.RowCol
 
 open Matrix
 variable(m n :ℕ)
-variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)(v₁: (Fin m) → ℝ )(i: Fin m)
+variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)(v₁: (Fin m) → ℝ )(i: Fin m)(k: Fin n)
+
 #check M *ᵥ v  -- works
 #check row v
 #check col v
 #check (row v₁) * M
+#check M i
 #check col (M i)
+
 
 variable (A : Matrix (Fin n) (Fin n) ℝ)
 variable (S : Set ((Fin n) → ℝ)) (hS : S = {v | A *ᵥ v = 0})
+
+variable (B: Set (Matrix (Fin n) Unit ℝ))(hB : B = {v | ∃ m₁, v = col (M m₁)})
 -- def S := {v | A *ᵥ v = 0}
 #check {v | ∃j, col (A j) = v}
 #check S
