1687a3bf16fdb94db9e979c3c44d4db184076b83
KatWei67 <jwei7@uw.edu>
2024-04-17 14:20:58 -0700

    define Farkas' Lemma

--- LinearProgramming/kat.lean
+++ LinearProgramming/kat.lean
@@ -3,4 +3,42 @@ import Mathlib.Data.Matrix.Basic
 import Mathlib.Data.Matrix.Reflection
 import Mathlib.Data.Matrix.RowCol
 
+import Mathlib.Data.Matrix.Notation
+import Mathlib.Data.Real.Basic
 -- check
+
+open Matrix
+
+variable(m n :ℕ)
+variable (M : Matrix (Fin m) (Fin n) ℝ) (v : (Fin n) → ℝ)(v₁: (Fin m) → ℝ )(i: Fin m)(j: Fin n)
+#check M *ᵥ v  -- works
+#check row v
+#check col v
+#check (row v₁) * M
+#check col (M i)
+#check row (M i)
+
+variable (A : Matrix (Fin n) (Fin n) ℝ)
+variable (S : Set ((Fin n) → ℝ)) (hS : S = {v | A *ᵥ v = 0})
+-- def S := {v | A *ᵥ v = 0}
+#check {v | ∃j, col (A j) = v}
+#check S
+
+
+-- Farkas' Lemma
+-- Define mathematical conditions
+variables {m n : ℕ}
+variables (A : Matrix (Fin m) (Fin n) ℝ) (c : (Fin n) → ℝ)
+
+def all_non_positive (v : (Fin m) → ℝ) : Prop :=
+  ∀ i, v i ≤ 0
+
+def all_non_negative (v : (Fin m) → ℝ) : Prop :=
+  ∀ i, v i ≥ 0
+
+-- Define the conditions of Farkas' Lemma
+def system_one_has_solution (A : Matrix (Fin m) (Fin n) ℝ) (c : (Fin n) → ℝ) : Prop :=
+  ∃ x : (Fin n) → ℝ, all_non_positive (A *ᵥ x) ∧ c * x > 0
+
+def system_two_has_solution (A : Matrix (Fin m) (Fin n) ℝ) (c : (Fin n) → ℝ) : Prop :=
+  ∃ lambda : (Fin m) → ℝ, (transpose A) *ᵥ lambda = c ∧ all_non_negative lambda
